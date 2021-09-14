99775f8d4bca55bb203651351512643985a8bc24
Víctor Cuadrado Juan <me@viccuad.me>
2021-09-14 09:46:16 +0200

    Comment custom faces until doom-monokai is fixed

--- config.el
+++ config.el
@@ -73,11 +73,11 @@
 (custom-set-faces!
   ;; simplify molokai/monokais:
   ;; bg-alt is taken by markdown code blocks
-  `(default :background ,(doom-darken 'bg-alt 0.3)) ;; bg
-  `(hl-line :background ,(doom-lighten 'bg-alt 0.1))  ;; base3
-  `(vertical-border :background ,(doom-darken 'bg-alt 0.3)) ;; same as default
-  `(fill-column-indicator :foreground ,(doom-color 'bg-alt)) ;; subtle indicator
-  `(treemacs-git-modified-face :foreground ,(doom-color 'cyan))
+;;  `(default :background ,(doom-darken 'bg-alt 0.3)) ;; bg
+;;  `(hl-line :background ,(doom-lighten 'bg-alt 0.1))  ;; base3
+;;  `(vertical-border :background ,(doom-darken 'bg-alt 0.3)) ;; same as default
+;;  `(fill-column-indicator :foreground ,(doom-color 'bg-alt)) ;; subtle indicator
+;;  `(treemacs-git-modified-face :foreground ,(doom-color 'cyan))
   ;; fix solaire for molokai:
   ;; `(solaire-default-face :background ,(doom-darken 'base3 0.5)) ;; darker text background
   ;; italics:
